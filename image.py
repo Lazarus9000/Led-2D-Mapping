a91b0e4bba831d60cc37357bf85668545de3dafa
Lazarus9000 <jegharen@gmail.com>
2023-12-28 21:48:24 +0100

    new bangbang

--- image.py
+++ image.py
@@ -4,17 +4,22 @@ import numpy as np
 import wledAPI
 import sys
 
-#Call e.g. python flag.py spiral.png to project image onto LEDS
+#Call script e.g. "python flag.py spiral.png" to project image onto LEDS
 
 imagepath = "dkdark.png"
+
+#Read image path from args
 if len(sys.argv) == 2:
     imagepath = sys.argv[1]
 
+#Initiate wledMapper
 wledAPI.wled("192.168.1.172", 450, "UDP", "calibration.txt")
 
 image = np.zeros((400, 400, 3), dtype='uint8')
 
+#Load image
 inputimg = cv2.imread(imagepath)
+#Blur to avoid aliasing when projecting onto the lower resolution LEDs
 img2 = cv2.GaussianBlur(inputimg,(0,0),10,cv2.BORDER_DEFAULT)
 
 wledAPI.image(img2,50)
